1b5991c020baf899ebca1bbf4eeabb35fcdc790f
yo0x <6yonor@gmail.com>
2017-06-22 15:51:29 +0300

    Some RealPython samples

---
+++ break.py
@@ -0,0 +1,10 @@
+def jump3():
+    for i in range(1, 50):
+        vo0 = 3 % i
+        if vo0 == 0:
+            continue
+        else:
+            print(i)
+
+
+jump3()
--- logic.py
+++ logic.py
@@ -6,4 +6,17 @@ def diinter(iint):
             print("{} is a divisor of {}".format(i, iint))
 
 
-diinter(iint)
+# diinter(iint)
+
+vinput = "s"
+
+
+def repin(vinput):
+    while True:
+        vinput = input("Please input some Again ")
+        if vinput == "q":
+            print("You pressesd Q exiting..!!bye3")
+            break
+
+
+repin(vinput)
