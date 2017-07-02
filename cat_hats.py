7b0ad473fad5c0e95624cb000f2c36d7633269f3
y9x <6yonor@gmail.com>
2017-07-02 15:42:15 +0300

    Some RealPython samples

---
+++ cat_hats.py
@@ -0,0 +1,14 @@
+cats = {'': ''}
+e = "0"
+for i in range(1, 101):
+    e = str(i)
+    cats["cat" + e] = "No Hat"
+
+for rounds in range(1, 100):
+    wround = rounds
+    if wround == 1 or wround == 100:
+        wround += 1
+        while True:
+            cats[cats]
+
+print(cats)
