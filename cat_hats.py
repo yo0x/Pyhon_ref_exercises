04b6eae2ba3f40ae8e6f8015d64b0e876058137a
y9x <6yonor@gmail.com>
2017-07-03 12:23:38 +0300

    Some RealPython samples

--- cat_hats.py
+++ cat_hats.py
@@ -1,18 +1,22 @@
 cats = {'': ''}
 e = "0"
-for i in range(1, 11):
+ccround1 = 0
+ccround2 = 0
+ccround3 = 0
+for i in range(1, 101):
     e = str(i)
     cats["cat" + e] = "No Hat"
 
-for rounds in range(0, 11):
+for rounds in range(0, 101):
     wround = rounds
-    ccround = 1
     if rounds == 0:
         continue
-    if wround == 1:
+    if wround == 1 or ccround1 == 1:
         wround += 1
-        ccround += 1
-        for imnum in range(0, 11, 2):
+        ccround2 = 1
+        ccround1 = 0
+        ccround3 = 0
+        for imnum in range(0, 101):
             nc = str(imnum)
             ncint = int(nc)
             bncint = ncint
@@ -20,10 +24,12 @@ for rounds in range(0, 11):
                 bncint += 1
                 cats["cat" + nc] = 'Hat'
                 continue
-        print(cats)
-    elif (ccround == ccround + 1):
-        ccround += 1
-        for imnum2 in range(0, 10, 3):
+            #        print(cats)
+    elif (ccround2 == 1):
+        ccround2 = 0
+        ccround1 = 0
+        ccround3 = 1
+        for imnum2 in range(0, 101, 2):
             nc2 = str(imnum2)
             ncint2 = int(nc2)
             bncint2 = ncint2
@@ -35,6 +41,23 @@ for rounds in range(0, 11):
                     cats["cat" + nc2] = 'No Hat'
                 else:
                     cats["cat" + nc2] = 'Hat'
+                continue
+    elif (ccround3 == 1):
+        ccround2 = 0
+        ccround1 = 1
+        ccround3 = 0
+        for imnum3 in range(0, 101, 3):
+            nc3 = str(imnum3)
+            ncint3 = int(nc3)
+            bncint3 = ncint3
+            if imnum3 == 0:
+                continue
+            while bncint3 == ncint3:
+                bncint3 += 1
+                if cats["cat" + nc3] == 'Hat':
+                    cats["cat" + nc3] = 'No Hat'
+                else:
+                    cats["cat" + nc3] = 'Hat'
                 continue
 
 print(cats)
