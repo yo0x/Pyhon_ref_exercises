7187b921a30632454c8de77d7e73a6cb12a61edd
y9x <6yonor@gmail.com>
2017-07-03 11:12:13 +0300

    Some RealPython samples

--- cat_hats.py
+++ cat_hats.py
@@ -1,14 +1,40 @@
 cats = {'': ''}
 e = "0"
-for i in range(1, 101):
+for i in range(1, 11):
     e = str(i)
     cats["cat" + e] = "No Hat"
 
-for rounds in range(1, 100):
+for rounds in range(0, 11):
     wround = rounds
-    if wround == 1 or wround == 100:
+    ccround = 1
+    if rounds == 0:
+        continue
+    if wround == 1:
         wround += 1
-        while True:
-            cats[cats]
+        ccround += 1
+        for imnum in range(0, 11, 2):
+            nc = str(imnum)
+            ncint = int(nc)
+            bncint = ncint
+            while bncint == ncint:
+                bncint += 1
+                cats["cat" + nc] = 'Hat'
+                continue
+        print(cats)
+    elif (ccround == ccround + 1):
+        ccround += 1
+        for imnum2 in range(0, 10, 3):
+            nc2 = str(imnum2)
+            ncint2 = int(nc2)
+            bncint2 = ncint2
+            if imnum2 == 0:
+                continue
+            while bncint2 == ncint2:
+                bncint2 += 1
+                if cats["cat" + nc2] == 'Hat':
+                    cats["cat" + nc2] = 'No Hat'
+                else:
+                    cats["cat" + nc2] = 'Hat'
+                continue
 
 print(cats)
