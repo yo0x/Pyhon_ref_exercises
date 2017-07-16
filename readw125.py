382709c17c2d06a161d3be3f9a123f65726324c4
y9x <6yonor@gmail.com>
2017-07-16 15:42:42 +0300

    Some RealPython samples

--- readw125.py
+++ readw125.py
@@ -1,21 +1,7 @@
-"""
-bpath = "/home/y9x/Desktop/test22.txt"
-my_lines = open(bpath, "r")
-for i in my_lines.readlines():
-    print("-2-" "-1-", i),
-my_lines.close()
-
-import os
-my_path = "/home/y9x/Desktop/files"
-file_names_list = os.listdir(my_path)
-for file_name in file_names_list:
-    if file_name.lower().endswith(".txt"):
-        print('Converting {} to JPG...'.format(file_name))
-        full_file_name = os.path.join(my_path, file_name)
-        new_file_name = full_file_name[0:len(full_file_name)-4] +".jpeg"
-        os.rename(full_file_name, new_file_name)
-""""
-import os
-
-fpath = "/var/home/y9x/Desktop/images/"
-file_name_list = os.listdir(fpath)
+vpathli = "/home/y9x/Desktop/images/text.txt"
+vpathli2 = "/home/y9x/Desktop/images/text2.txt"
+with open(vpathli, "r") as my_inputf, open(vpathli2, "w") as my_outputf:
+    for line in my_inputf.readlines():
+        my_outputf.writelines(line)
+        print(line)
+my_inputf.close()
