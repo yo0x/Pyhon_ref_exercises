f368cd4eb809676748d008500243fccf3c5ddb16
y9x <6yonor@gmail.com>
2017-07-20 14:57:17 +0300

    Some RealPython samples

--- readw125.py
+++ readw125.py
@@ -5,3 +5,7 @@ with open(vpathli, "r") as my_inputf, open(vpathli2, "w") as my_outputf:
         my_outputf.writelines(line)
         print(line)
 my_inputf.close()
+my_inputf = open(vpathli2, "r")
+my_inputf.seek(5, 0)
+print("Line 2", my_inputf.readline())
+my_inputf.close()
